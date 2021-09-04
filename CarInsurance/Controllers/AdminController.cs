a450230a5fe3ae530c4ec5da9cf6f0c27fcc3b91
David <dcmswim@gmail.com>
2021-09-04 16:08:54 -0700

    Add project files.

---
+++ CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = (from c in db.Insurees where c.Quote > 0 select c).ToList();
+                var insureeVms = new List<InsureeVm>();
+                foreach (var insuree in insurees)
+                {
+                    var insureeVm = new InsureeVm();
+                    insureeVm.FirstName = insuree.FirstName;
+                    insureeVm.LastName = insuree.LastName;
+                    insureeVm.EmailAddress = insuree.EmailAddress;
+                    insureeVms.Add(insureeVm);
+                }
+
+                return View(insureeVms);
+            }
+        }
+    }
+}
---
+++ CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+
+    public class InsureeController : Controller
+    {
+        //introduces multipliers that may be applied in future logic
+        private const decimal chargeDUI = 1.25m;
+        private const decimal fullCoverage = 1.50m;
+
+        //function for calculating quote based on input
+        public decimal CalculateQuote(Insuree insuree)
+        {
+            //sets base quote at 50 
+            decimal quote = 50;
+
+            DateTime current = DateTime.Now;
+            //subtracts current year from DoB year to get age
+            int age = current.Year - insuree.DateOfBirth.Year;
+            //adds to quote depending on age of user
+            if (age < 18)
+            {
+                quote += 100;
+            }
+            else if (age > 18 && age < 26)
+            {
+                quote += 50;
+            }
+            else quote += 25;
+            //adds to quote based on year of car
+            if (insuree.CarYear < 2000 || insuree.CarYear > 2015)
+            {
+                quote += 25;
+            }
+            //adds to quote based on make
+            if (insuree.CarMake == "Porsche")
+            {
+                quote += 25;
+                //if previous condition and next condition are true adds to quote
+                if (insuree.CarModel == "911 Carrera")
+                {
+                    quote += 25;
+                }
+            }
+            //adds to quote based on number of speeding tickets
+            if (insuree.SpeedingTickets > 0)
+            {
+                quote += (insuree.SpeedingTickets * 10);
+            }
+            //adds to quote if DUI is true
+            if (insuree.DUI)
+            {
+                quote *= chargeDUI;
+            }
+            //adds to quote if full coverage indicated
+            if (insuree.CoverageType)
+            {
+                quote *= fullCoverage;
+            }
+            return quote;
+
+        }
+
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                insuree.Quote = CalculateQuote(insuree);
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
